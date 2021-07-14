6b32799ef891f095da4caa5fa1fce9979cb78a93
DanielTongAwesome <danieltongubc@gmail.com>
2021-07-13 23:16:01 -0400

    [ENTF-81] #1 update contract to permit contract

--- contracts/Entropy.sol
+++ contracts/Entropy.sol
@@ -1,10 +1,11 @@
 // SPDX-License-Identifier: GPL 3.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract Entropy is ERC20 {
-    constructor() ERC20("Entropy", "ERP") {
+contract Entropy is ERC20, ERC20Permit {
+    constructor() ERC20("Entropy", "ERP") ERC20Permit("Entropy") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
